d09f6dd4cb99c789c17a08c07768f38e7329382f
embe8 <eebon@icloud.com>
2026-03-01 23:33:23 -0800

    Add bootup container

--- vite-desktop-simulator/src/pages/Bootup.jsx
+++ vite-desktop-simulator/src/pages/Bootup.jsx
@@ -3,6 +3,11 @@ import '././Bootups.css';
 
 function Bootup() {
     return (
-        
+        <div className="bootup-container">
+            <div className="bootup-content">
+                <h1>Booting up...</h1>
+            </div>
+        </div>
     );
 }
+export default Bootup;
