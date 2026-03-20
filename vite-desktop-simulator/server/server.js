64e30aae02ee1507ee952024473386ee36be8896
embe8 <eebon@icloud.com>
2026-03-19 23:22:33 -0700

    Add sequelize to create database tables from models

--- vite-desktop-simulator/server/server.js
+++ vite-desktop-simulator/server/server.js
@@ -14,3 +14,27 @@
  app.use(express.json());
 //Serve images from the images folder
  app.use('/images/', express.static(path.join(__dirname, 'images')));
+
+ // Serve static files from the dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all route to serve index.html for any unmatched routes
+app.get('*', (req, res) => {
+    const indexPath = path.join(__dirname, 'dist', 'index.html');
+    if (fs.existsSync(indexPath)) {
+      res.sendFile(indexPath);
+    } else {
+      res.status(404).send('index.html not found');
+    }
+  });
+
+  // Error handling middleware
+/* eslint-disable no-unused-vars */
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+  });
+  /* eslint-enable no-unused-vars */
+  
+  // Sync database and load default data if none exist
+await sequelize.sync();
